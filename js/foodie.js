31ca073307d5389d84a5ccb7a590af8f17a738a6
Julieta Hang <julieta.m.hang@gmail.com>
2016-08-25 14:08:12 -0400

    Foodie Page finished. Probelm with 'things I like' page

--- js/foodie.js
+++ js/foodie.js
@@ -1,3 +1,5 @@
+var scrollTime = 500;
+
 $(document).ready(function(){
 	$('.carousel').carousel({
 	 interval: 1500
@@ -18,13 +20,12 @@ $(document).ready(function(){
 		e.preventDefault();
 		$("body").removeClass("aboutbox-on");
 		
-		})
+	});
 		
-
+//pics of NY//
 	$(".meal").on("click",function(e){
 	    e.preventDefault();
 	      var scrollTime = 500;
-	    
 	      if( $(this).hasClass("link_brunch") ) {
 	        $.scrollTo("#brunch_places",scrollTime,{
 	      
@@ -39,13 +40,12 @@ $(document).ready(function(){
 
 	});
 
-	$(".meal").on("click",function(e){
+//pics of SF//
+	$(".place").on("click",function(e){
 	    e.preventDefault();
-	      var scrollTime = 500;
 	    
 	      if( $(this).hasClass("link_mission") ) {
 	        $.scrollTo("#mission_places",scrollTime,{
-	      
 	        });
 	      } else if( $(this).hasClass("link_hayes") ) {
 	        $.scrollTo("#hayes_places",scrollTime);
@@ -70,9 +70,34 @@ $(document).ready(function(){
 	  
 	    }
 
-
 	});
+
+	//click listener for arrows//
+
+	$(".emph.menu").on("click",function(){
+		console.log(this,this.id)
+		if(this.id=="nycarrow"){
+			$.scrollTo(".page2",scrollTime);
+		}else if (this.id=="sfoarrow"){
+			$.scrollTo(".page7",scrollTime);
+		}else if (this.id=="toparrow"){
+			$.scrollTo(".whitewall",scrollTime);
+		}	
+	});
+//arrow to NYC menu//	
 	$(window).scroll(function(){
+		if(window.scrollY>=1300 && window.scrollY<9695){
+			$("#nycarrow").addClass("stickyarrow")
+		}else{
+			$("#nycarrow").removeClass("stickyarrow")
+		}
+//arrow to SFO menu//
+		if(window.scrollY>=10670){
+			$("#sfoarrow").addClass("stickyarrow")
+		}else{
+			$("#sfoarrow").removeClass("stickyarrow")
+		}
+//arrow to landing page menu//
 		if(window.scrollY>=1300){
 			$("#toparrow").addClass("stickyarrow")
 		}else{
