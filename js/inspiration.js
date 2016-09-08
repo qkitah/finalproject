634a6ab83fa1d5aa4b05ccb6da0d1f27f5082db4
Julieta Hang <julieta.m.hang@gmail.com>
2016-09-08 16:46:35 -0400

    corrected wording in coffees and added new inspiration

--- js/inspiration.js
+++ js/inspiration.js
@@ -19,4 +19,38 @@ $(document).ready(function(){
 		$("body").removeClass("aboutbox-on");
 		
 	});
+
+	$(".menu").on("click",function(e){
+		e.preventDefault();
+		var scrollTime = 500;
+ 		// link_home should point to firstpage
+ 		//link_about should point to second page
+  		//link_expertise should point to third page
+  		if( $(this).hasClass("link_home") ){
+  			$.scrollTo(".flower", scrollTime,{
+  			offset: -100
+  			});
+  		} else if ($(this).hasClass("link_about") ){
+  			$.scrollTo(".secondpage",scrollTime)
+  		} else if ($(this).hasClass("link_skills") ){
+  			$.scrollTo(".thirdpage",scrollTime)
+  		}else if ($(this).hasClass("link_thingsILike") ){
+        $.scrollTo(".forthpage",scrollTime)
+      }else if ($(this).hasClass("link_contact") ){
+        $.scrollTo(".fifthpage",scrollTime)} 
+  	 }); 	
+
+
+	$(window).on("scroll",function(){
+	if( window.scrollY > 500) {
+       // $("#social_media").css("position","absolute");
+       // $("#social_media").css("top","420px");
+       $("#toparrow").addClass("stickyarrow");
+     } else { //window.scrollY >= 403
+       // $("#social_media").css("position", "fixed");
+       // $("#social_media").css("top","36px");
+       $("#toparrow").removeClass("stickyarrow");
+     }
+   });
+
 });	
